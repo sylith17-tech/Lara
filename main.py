3123f337f473b30a7e3ada3f5f9f7ea9cdff829b
VIP_ARM <vip_arm@kernel-0x0.com>
2026-09-01 12:55:30 +0000

    fix synchronous event loop for async updates

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from telegram.ext import Application
 from flask import Flask, request
+from telegram import Update
 import asyncio
 
 TOKEN = os.environ.get('BOT_TOKEN', '8927003617:AAEyXIlPMr3zjA8M9TWA6znywR9kM4ANWJQ')
@@ -11,17 +12,19 @@ application = Application.builder().token(TOKEN).build()
 
 @app.route(f'/{TOKEN}', methods=['POST'])
 def webhook():
-    json_data = request.get_json(force=True)
-    from telegram import Update
-    update = Update.de_json(json_data, application.bot)
-    asyncio.run(application.process_update(update))
-    return 'OK', 200
+    if request.headers.get('content-type') == 'application/json':
+        json_data = request.get_json(force=True)
+        update = Update.de_json(json_data, application.bot)
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(application.initialize())
+        loop.run_until_complete(application.process_update(update))
+        return 'OK', 200
+    return 'Forbidden', 403
 
 @app.route('/')
 def index():
     return 'Lara Bot is Alive!', 200
 
 if __name__ == '__main__':
-    application.initialize()
-    application.bot.set_webhook(url=f'https://lara-iecv.onrender.com/{TOKEN}')
     app.run(host='0.0.0.0', port=PORT)
